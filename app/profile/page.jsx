962801559c32ead0ef45f49e21134c64fada3c34
brianserem <brianseremiat@gmail.com>
2023-08-19 22:35:14 +0300

    fixed typo

--- app/profile/page.jsx
+++ app/profile/page.jsx
@@ -1,7 +1,7 @@
 'use client'
 import { useState, useEffect } from 'react'
 import { useSession } from 'next-auth/react'
-import Profile from '@components/profile'
+import Profile from '@components/Profile'
 import { useRouter } from 'next/navigation'
 
 
