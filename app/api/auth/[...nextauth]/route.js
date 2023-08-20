8c86d5cbf8839d6275755e4c0874aebceb661fa8
brianserem <brianseremiat@gmail.com>
2023-08-20 11:15:32 +0300

    fixed typo in GOOGLE auth provider

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -4,16 +4,23 @@ import GoogleProvider from 'next-auth/providers/google';
 import User from '@models/user';
 import { connectToDB } from '@utils/database';
 
+
+console.log({
+  clientId: process.env.GOOGLE_ID,
+  clientSecret: process.env.GOOGLE_SECRET,
+})
+
 const handler = NextAuth({
   providers: [
     GoogleProvider({
       clientId: process.env.GOOGLE_ID,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+      clientSecret: process.env.GOOGLE_SECRET,
     })
   ],
   callbacks: {
     async session({ session }) {
       // store the user id from MongoDB to session
+      console.log(session)
       const sessionUser = await User.findOne({ email: session.user.email });
       session.user.id = sessionUser._id.toString();
 
